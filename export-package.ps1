2458a996580be0592006b167e01fe80c6c504643
Burke Azbill <burkeazbill@gmail.com>
2015-03-26 10:05:57 -0400

    Added Import and Export PowerShell Scripts authored by Chander Chandak and updated by Burke Azbill

---
+++ export-package.ps1
@@ -0,0 +1,57 @@
+Param(
+  [string]$vroHost="localhost",
+  [string]$vroPort="8281",
+  [string]$user="vcoadmin",
+  [string]$pass="vcoadmin",
+  [string]$exportPath="c:\",
+  [Parameter(Mandatory=$true)]
+  [string]$packageName='com.vmware.test',
+  [string]$fileName=$packageName + ".package"
+)
+
+#### Make no changes below this line ###############################
+# Usage:
+# If you run the script with no parameters specified, the default values defined above will be used.
+# to run with params, See following example: (Should be all one line)
+# NOTE: It is not required to specify name of each parameter, but order will need to match the order in the above params section
+# NOTE: Be sure exportPath includes trailing backslash
+# PS C:\> ./export-package.ps1 -vroHost vro6.demo.lab -vroPort 8281 -user vcoadmin -pass vcoadmin -exportPath "c:\hol\" -packageName await-tokens
+#
+####################################################################
+
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+function ConvertTo-Base64($string) {
+   $bytes  = [System.Text.Encoding]::UTF8.GetBytes($string);
+   $encoded = [System.Convert]::ToBase64String($bytes); 
+
+   return $encoded;
+}
+
+$vcoUrl = "https://$($vroHost):$($vroPort)/vco/api";
+
+# Authentication token
+$token = ConvertTo-Base64("$($user):$($pass)");
+$auth = "Basic $($token)";
+
+$headers = @{"Authorization"= $auth;'Accept'='Application/zip'; 'Accept-Encoding'='gzip, deflate'; };
+$expPackageURI = "https://$($vroHost):$($vroPort)/vco/api/packages/$($packageName)/?exportConfigurationAttributeValues=true";
+$ret = Invoke-WebRequest -uri $expPackageURI -Headers $headers -ContentType "application/zip;charset=utf-8" -Method Get
+#Start-Sleep -s 10
+
+$ret.Content | Set-Content -Path  $exportpath$fileName -Encoding Byte
+
+write-host "";
+write-host "$expPackageURI";
+write-host "Exported  to: $exportpath$fileName";
---
+++ import-package.ps1
@@ -0,0 +1,90 @@
+Param(
+  [string]$vroHost="localhost",
+  [string]$vroPort="8281",
+  [string]$user="vcoadmin",
+  [string]$pass="vcoadmin",
+  [Parameter(Mandatory=$true)]
+  [System.IO.FileInfo] $file
+ 
+)
+
+#### Make no changes below this line ###############################
+# Usage:
+# If you run the script with no parameters specified, the default values defined above will be used.
+# to run with params, See following example: (Should be all one line)
+# NOTE: It is not required to specify name of each parameter, but order will need to match the order in the above params section
+# PS C:\> ./import-package.ps1 -vroHost vro6.demo.lab -vroPort 8281 -user vcoadmin -pass vcoadmin -file c:\Packages\test.package
+#
+####################################################################
+
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+        
+[byte[]]$CRLF = 13, 10
+
+function Get-AsciiBytes([String] $str) {
+    return [System.Text.Encoding]::ASCII.GetBytes($str)            
+}
+
+function ConvertTo-Base64($string) {
+   $bytes  = [System.Text.Encoding]::UTF8.GetBytes($string);
+   $encoded = [System.Convert]::ToBase64String($bytes); 
+
+   return $encoded;
+}
+
+    
+$body = New-Object System.IO.MemoryStream
+
+$method = 'POST'
+$boundary = [Guid]::NewGuid().ToString().Replace('-','')
+$ContentType = 'multipart/form-data; boundary=' + $boundary
+$b2 = Get-AsciiBytes ('--' + $boundary)
+$body.Write($b2, 0, $b2.Length)
+$body.Write($CRLF, 0, $CRLF.Length)            
+            
+$b = (Get-AsciiBytes ('Content-Disposition: form-data; name="file"; filename="$($file.Name)";'))
+$body.Write($b, 0, $b.Length)
+$body.Write($CRLF, 0, $CRLF.Length)            
+$b = (Get-AsciiBytes 'Content-Type:application/octet-stream')
+$body.Write($b, 0, $b.Length)
+            
+$body.Write($CRLF, 0, $CRLF.Length)
+$body.Write($CRLF, 0, $CRLF.Length)
+            
+$b = [System.IO.File]::ReadAllBytes($file.FullName)
+$body.Write($b, 0, $b.Length)
+
+$body.Write($CRLF, 0, $CRLF.Length)
+$body.Write($b2, 0, $b2.Length)
+            
+$b = (Get-AsciiBytes '--');
+$body.Write($b, 0, $b.Length);
+            
+$body.Write($CRLF, 0, $CRLF.Length);
+            
+             
+
+# Authentication token
+$token = ConvertTo-Base64("$($user):$($pass)");
+$auth = "Basic $($token)";
+
+#request URL
+$impUrl = "https://$($vroHost):$($vroPort)/vco/api/packages/?overwrite=true";
+$header = @{"Authorization"= $auth;
+            "Accept"= "application/zip";
+            "Accept-Encoding"= "gzip,deflate,sdch";};                
+
+$impUrl
+Invoke-RestMethod -Method $method -Uri $impUrl -ContentType $ContentType -Body $body.ToArray() -Headers $header
